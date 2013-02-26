febe0a2e46f4012a3d397277bf57dbe2d189a549
Tim Oxley <secoif@gmail.com>
2013-02-26 15:23:48 +0800

    require does all this for .json files anyway

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 /*jslint node:true*/
-var fs = require('fs'),
-    path = require('path'),
-    HTTPStatusFile = path.resolve(__dirname + '/http_status.json'),
-    codes;
-
-codes = fs.readFileSync(HTTPStatusFile, 'UTF-8');
-module.exports = JSON.parse(codes);
+module.exports = require('./http_status.json')
